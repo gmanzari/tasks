40570f8094d8e976b03dfcdc4828795624033878
Gabi Manzari <gmanzari@udel.edu>
2025-09-16 15:11:53 -0400

    fixed publishQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -134,7 +134,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newPublishedState = !question.published;
+    const newQuestion = { ...question, published: newPublishedState };
+    return newQuestion;
 }
 
 /**
