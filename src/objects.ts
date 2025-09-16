86d8ccce9c734b95751657e6e203a5463c4a7f4f
Gabi Manzari <gmanzari@udel.edu>
2025-09-16 14:44:52 -0400

    fixed isCorrect function

--- src/objects.ts
+++ src/objects.ts
@@ -31,7 +31,12 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const expected = question.expected;
+    const expectedNoUpper = expected.toLowerCase();
+    const answerNoUpper = answer.toLowerCase();
+    const expectedTrimmed = expectedNoUpper.trim();
+    const answerTrimmed = answerNoUpper.trim();
+    return answerTrimmed === expectedTrimmed;
 }
 
 /**
