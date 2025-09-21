617b9a5d797a7170e6757af81919231433a97d2c
Gabi Manzari <gmanzari@udel.edu>
2025-09-21 18:28:45 -0400

    fixed toCSV function

--- src/nested.ts
+++ src/nested.ts
@@ -116,7 +116,20 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const stringVersion = questions.map(
+        (question: Question): string =>
+            question.id.toString() +
+            "," +
+            question.name +
+            "," +
+            question.options.length +
+            "," +
+            question.points +
+            "," +
+            question.published.toString(),
+    );
+    const allCombined = stringVersion.join("\n");
+    return "id,name,options,points,published\n" + allCombined;
 }
 
 /**
