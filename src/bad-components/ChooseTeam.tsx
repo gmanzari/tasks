50de120199c0014e36dee20425c0ca8d2f81524a
Gabi Manzari <gmanzari@udel.edu>
2025-09-28 20:59:45 -0400

    fixed ChooseTeam and ColoredBox Components

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
 
-const PEOPLE = [
-    "Alan Turing",
-    "Grace Hopper",
-    "Ada Lovelace",
-    "Charles Babbage",
-    "Barbara Liskov",
-    "Margaret Hamilton",
-];
-
 export function ChooseTeam(): React.JSX.Element {
+    const PEOPLE = [
+        "Alan Turing",
+        "Grace Hopper",
+        "Ada Lovelace",
+        "Charles Babbage",
+        "Barbara Liskov",
+        "Margaret Hamilton",
+    ];
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            setAllOptions([...team, newMember]);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
     }
 
     return (
@@ -36,7 +31,12 @@ export function ChooseTeam(): React.JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => {
+                                    chooseMember(option);
+                                }}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
